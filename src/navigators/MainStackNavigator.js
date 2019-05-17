f59d07c5bb102eb16034a957428838ea7ed1c8ae
Gustavo <gustavos60@gmail.com>
2019-05-17 19:18:04 -0300

    Removida barra superior do MainStackNavigator

--- src/navigators/MainStackNavigator.js
+++ src/navigators/MainStackNavigator.js
@@ -3,12 +3,12 @@ import ListScreen from '../screens/ListScreen'
 import { createStackNavigator, createAppContainer } from 'react-navigation'
 import MapScreen from '../screens/MapScreen';
 
-const MainStackNavigator = createStackNavigator ({
-    Main: MainScreen,
-    List: ListScreen,
-    Map: MapScreen
+const MainStackNavigator = createStackNavigator({
+  Main: { screen: MainScreen, navigationOptions: { header: null } },
+  List: { screen: ListScreen, navigationOptions: { header: null } },
+  Map: { screen: MapScreen, navigationOptions: { header: null } }
 }, {
     headerMode: 'screen'
-})
+  })
 
 export default createAppContainer(MainStackNavigator)
