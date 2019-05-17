90c67e2cf415046487b7e01c0e4cbdbd8e5be6b5
Gustavo <gustavos60@gmail.com>
2019-05-17 19:08:06 -0300

    Implementação minima do mapa interativo

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,10 +1,84 @@
 import React, { Component } from 'react'
-import { StyleSheet, Image, View, Text, TouchableOpacity } from 'react-native'
+import { StyleSheet, Image, View, Text, TouchableOpacity, ScrollView } from 'react-native'
 import SearchBar from '../components/SearchBar'
 import BottomBar from '../components/BottomBar'
 import { connect } from 'react-redux'
 
 class MapScreen extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      corredor: 0,
+    }
+  }
+
+  _renderMap = () => {
+    if (this.state.corredor === 0) {
+      return (
+        <View style={styles.image} >
+          <View style={styles.corredor}>
+          </View>
+          <TouchableOpacity
+            style={styles.corredorButton}
+            activeOpacity={0.6}
+            onPress={() => this.setState({ corredor: 1 })} >
+            <Text style={[styles.routeText, { fontSize: 40 }]} >01</Text>
+          </TouchableOpacity>
+          <View style={styles.corredor}>
+          </View>
+          <TouchableOpacity
+            style={styles.corredorButton}
+            activeOpacity={0.6}
+            onPress={() => this.setState({ corredor: 1 })} >
+            <Text style={[styles.routeText, { fontSize: 40 }]} >02</Text>
+          </TouchableOpacity>
+          <View style={styles.corredor}>
+          </View>
+          <TouchableOpacity
+            style={styles.corredorButton}
+            activeOpacity={0.6}
+            onPress={() => this.setState({ corredor: 1 })} >
+            <Text style={[styles.routeText, { fontSize: 40 }]} >03</Text>
+          </TouchableOpacity>
+          <View style={styles.corredor}>
+          </View>
+        </View>
+      )
+    } else {
+      return (
+        <View style={styles.image} >
+          <View style={styles.corredor}>
+          </View>
+          <View style={{ justifyContent: 'space-around', height: '100%' }} >
+            <TouchableOpacity style={styles.item} >
+              <Text style={{ color: 'white' }} > {'<-'} Copos</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.item} >
+              <Text style={{ color: 'white' }} > {'<-'} Pratos</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.item} >
+              <Text style={{ color: 'white' }} > {'<-'} Balões</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.corredor}>
+          </View>
+        </View>
+      )
+    }
+  }
+
+  _buttonText = () => {
+    if (this.state.corredor === 0){
+      return (
+        <Text style={styles.routeText} >Traçar rota</Text>
+      )
+    } else {
+      return (
+        <Text style={styles.routeText} >Retornar ao mapa</Text>
+      )
+    }
+  }
 
   render() {
     return (
@@ -14,11 +88,16 @@ class MapScreen extends Component {
           <Text style={styles.storeText} >Atacado dos Presentes</Text>
         </View>
         <View style={styles.container} >
-          <TouchableOpacity activeOpacity={0.7} style={styles.rota} >
-            <Text style={styles.routeText} >Traçar rota</Text>
+          <TouchableOpacity
+            activeOpacity={0.7}
+            style={styles.rota}
+            onPress={() => this.setState({ corredor: 0 })}>
+            {this._buttonText()}
+            {/* <Text style={styles.routeText} >Traçar rota</Text> */}
           </TouchableOpacity>
         </View>
-        <Image style={styles.image} resizeMode='contain' source={require('../../assets/images/mapa.png')} />
+        {this._renderMap()}
+        {/* <Image style={styles.image} resizeMode='contain' source={require('../../assets/images/mapa.png')} /> */}
         <TouchableOpacity
           style={styles.indicator}
           onPress={() => this.props.navigation.navigate('List')}
@@ -52,8 +131,37 @@ const styles = StyleSheet.create({
   },
   image: {
     flex: 3,
-    marginRight: 15,
-    marginLeft: 15
+    margin: 15,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'scroll',
+  },
+  corredor: {
+    backgroundColor: '#666666',
+    height: '100%',
+    width: 15,
+    borderRadius: 10,
+  },
+  corredorButton: {
+    backgroundColor: '#47B036',
+    borderRadius: 25,
+    color: 'white',
+    width: 75,
+    height: 75,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  item: {
+    backgroundColor: '#47B036',
+    height: 30,
+    width: 75,
+    borderRadius: 15,
+    margin: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   storeText: {
     color: 'white',
