ae800c749ceaa925b0fa6a9ae6d9c22f217344f9
Branco <mbs8@cin.ufpe.br>
2019-05-18 10:48:31 -0300

    Conserta o bug da tela inicial ter desparecido por causa do tabNavigator

--- src/navigators/StackNavigator.js
+++ src/navigators/StackNavigator.js
@@ -1,16 +1,10 @@
 import HomeScreen from '../screens/HomeScreen'
-import MainScreen from '../screens/MainScreen'
-import ListScreen from '../screens/ListScreen'
-import StoresScreen from '../screens/StoresScreen'
-import MapScreen from '../screens/MapScreen'
 import { createStackNavigator, createAppContainer } from 'react-navigation'
+import TabNavigator from './TabNavigator';
 
 const StackNavigator = createStackNavigator({
   Home: { screen: HomeScreen, navigationOptions: { header: null } },
-  Main: { screen: MainScreen, navigationOptions: { header: null } },
-  List: { screen: ListScreen, navigationOptions: { header: null } },
-  Store: { screen: StoresScreen, navigationOptions: { header: null } },
-  Map: { screen: MapScreen, navigationOptions: { header: null } },
+  Store: { screen: TabNavigator, navigationOptions: { header: null } },
 }, {
     headerMode: 'screen'
   })
