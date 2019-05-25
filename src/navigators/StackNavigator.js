7ce6dd722d26774491d31933d9257946a300a53b
Branco <mbs8@cin.ufpe.br>
2019-05-25 18:33:39 -0300

    Resolve pequenos bugs da navegação

--- src/navigators/StackNavigator.js
+++ src/navigators/StackNavigator.js
@@ -1,12 +1,14 @@
 import HomeScreen from '../screens/HomeScreen'
 import { createStackNavigator, createAppContainer } from 'react-navigation'
 import TabNavigator from './TabNavigator';
+import MapStackNavigator from './MapStackNavigator';
 
 const StackNavigator = createStackNavigator({
   Home: { screen: HomeScreen, navigationOptions: { header: null } },
   Store: { screen: TabNavigator, navigationOptions: { header: null } },
+  Map: {screen: MapStackNavigator, navigationOptions: {header: null } }
 }, {
-    headerMode: 'screen'
+    headerMode: 'screen',
   })
 
 export default createAppContainer(StackNavigator)
