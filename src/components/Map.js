da6d541662741630f493c24e673e6a89d1c49962
Gustavo <gustavos60@gmail.com>
2019-05-27 19:11:45 -0300

    Ajuste no calculo dinamico da altura dos componentes

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,16 +5,30 @@ import Linha from './Linha'
 
 export default class Map extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      height: 0
+    }
+  }
+
   render() {
 
-    const itemHeight = '' + 100 / mapa.length + '%'
+    let itemHeight = this.state.height / mapa.length
     return (
-      <View style={styles.container}>
+      <View style={styles.container} onLayout={(event) => {
+        let { height } = event.nativeEvent.layout
+        this.setState({ height })
+      }} >
         <FlatList
           data={mapa}
           contentContainerStyle={{ flexGrow: 1 }}
           keyExtractor={(item, index) => '' + index}
-          renderItem={(linha) => <Linha atualizaCorredor={this.props.atualizaCorredor} itemHeight={itemHeight} dado={linha.item} />}
+          renderItem={(linha) =>
+            <Linha
+              atualizaCorredor={this.props.atualizaCorredor}
+              itemHeight={itemHeight}
+              dado={linha.item} />}
         />
       </View >
     )
