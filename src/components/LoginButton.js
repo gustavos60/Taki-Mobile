dfb4f96bfffe6d660cfbcbb48668f739593530f0
Branco <mbs8@cin.ufpe.br>
2019-05-27 11:15:48 -0300

    Adiciona props para o componente login button e altera o login na HomeScreen e o ícone do botão ir às compras na ListScreen

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -6,16 +6,17 @@ export default class LoginButton extends Component {
 
   render() {
 
-    let backgroundColor = (this.props.color) ? this.props.color : '#3b5998'
+    let backgroundColor = (this.props.color) ? this.props.color : 'black'
     let textColor = (this.props.textColor) ? this.props.textColor : 'white'
+    let iconName = (this.props.iconName) ? this.props.iconName : 'set-none'
 
     return (
       <TouchableOpacity
-        //activeOpacity={0.7}
+        activeOpacity={0.7}
         style={[styles.container, { backgroundColor: backgroundColor }]}
         onPress={() => this.props.onPress()}>
 
-        <Icon style={[styles.icon, { color: textColor }]} size={25} name='facebook'/>
+        <Icon style={[styles.icon, { color: textColor }]} size={25} name={iconName}/>
         <Text style={[styles.text, { color: textColor }]} >{this.props.text}</Text>
 
       </TouchableOpacity>
@@ -28,7 +29,6 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     width: '70%',
-    backgroundColor: '#3b5998',
     height: 40,
     alignItems: 'center',
     justifyContent: 'space-around',
