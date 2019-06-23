1ace0a20ff32f50d8340607556c6303672cb5fa7
Gustavo <gustavos60@gmail.com>
2019-06-23 12:15:52 -0300

    Adicionado lógica do Redux para guardar usuário apos login

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,9 +2,11 @@ import { categoriaReducer } from './categoriaReducer'
 import { itemReducer } from './itemReducer'
 import { combineReducers } from 'redux'
 import { rotaReducer } from './rotaReducer'
+import { userReducer } from './userReducer'
 
 export const Reducers = combineReducers({
   categoriaState: categoriaReducer,
   itemState: itemReducer,
-  rotaState: rotaReducer
+  rotaState: rotaReducer,
+  userState: userReducer
 })
