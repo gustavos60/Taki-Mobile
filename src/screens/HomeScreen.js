d8fb3e4d8276610754ccfb6ba6ddc57fce523c75
Gustavo <gustavos60@gmail.com>
2019-06-23 12:16:04 -0300

    Fazendo login com google e salvando usuário no redux

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react'
-import { StyleSheet, Image } from 'react-native'
+import { StyleSheet, Image, Alert } from 'react-native'
 import LinearGradient from 'react-native-linear-gradient'
 import LoginButton from '../components/LoginButton'
+import { GoogleSignin, statusCodes } from 'react-native-google-signin'
+import { connect } from 'react-redux'
+import { addUser } from '../redux/actions/userActions'
 
-export default class HomeScreen extends Component {
+class HomeScreen extends Component {
+
+  signIn = async () => {
+    try {
+      await GoogleSignin.configure({
+        scopes: ['https://www.googleapis.com/auth/drive.readonly'], 
+        webClientId: '206610878188-2urtn9te6pua4h1r7sgtbd220rtfk43e.apps.googleusercontent.com',  
+        forceConsentPrompt: true, 
+      })
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn()
+      console.log(userInfo)
+      this.props.addUser(userInfo)
+      this.props.navigation.navigate('Store')
+    } catch (error) {
+      Alert.alert('Erro de autenticação', JSON.stringify(error))
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (f.e. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
 
   render() {
     return (
@@ -13,20 +42,22 @@ export default class HomeScreen extends Component {
           style ={styles.icon} text="Continuar com Google" 
           iconName='google'
           color='#3270f0'
-          onPress={() => this.props.navigation.navigate('Store')} 
+          onPress={() => this.signIn()} 
         />
       </LinearGradient>
     )
   }
 }
-/*
-<LoginButton 
-          text="Continuar com Facebook" 
-          color='#3b5998'
-          iconName='facebook'
-          onPress={() => this.props.navigation.navigate('Store')} 
-        />
-*/
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addUser: (user) => dispatch(addUser(user))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(HomeScreen)
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
