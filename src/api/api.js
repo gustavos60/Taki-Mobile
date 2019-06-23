ea144825f82e0da466bde51ccfc95f43a8827604
Gustavo <gustavos60@gmail.com>
2019-06-23 15:31:13 -0300

    Exemplo de chamada de api usando axios

--- src/api/api.js
+++ src/api/api.js
@@ -4,4 +4,8 @@ const api = axios.create({
   baseURL: 'http://157.230.177.36:8080',
 })
 
+export const addToken = async (token) => {
+  api.defaults.headers.common['Authorization'] = 'Bearer ' + token
+}
+
 export default api
