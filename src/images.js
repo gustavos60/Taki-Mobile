f4d63635812eafe223bd53b74095de9e8bcf2fa9
Gustavo <gustavos60@gmail.com>
2019-06-28 15:09:58 -0300

    Atualizado array com imagens

--- src/images.js
+++ src/images.js
@@ -1,10 +1,202 @@
 export const images = [
-  require('../assets/images/pratos.png'),
-  require('../assets/images/postit.png'),
-  require('../assets/images/papela4.png'),
-  require('../assets/images/pastas.png'),
-  require('../assets/images/grampos.png'),
-  require('../assets/images/copos.png'),
-  require('../assets/images/baloes.png'),
-  require('../assets/images/confeti.png'),
+  {
+    id: 9,
+    image: require('../assets/images/produtos/descartaveis9.png')
+  },
+  {
+    id: 12,
+    image: require('../assets/images/produtos/biscoitos12.jpg')
+  },
+  {
+    id: 13,
+    image: require('../assets/images/produtos/bebidas13.png')
+  },
+  {
+    id: 14,
+    image: require('../assets/images/produtos/ovos14.png')
+  },
+  {
+    id: 15,
+    image: require('../assets/images/produtos/hortifruti15.jpg')
+  },
+  {
+    id: 16,
+    image: require('../assets/images/produtos/paes16.jpeg')
+  },
+  {
+    id: 17,
+    image: require('../assets/images/produtos/achocolatados17.jpeg')
+  },
+  {
+    id: 18,
+    image: require('../assets/images/produtos/bolo18.jpg')
+  },
+  {
+    id: 19,
+    image: require('../assets/images/produtos/frutas19.jpeg')
+  },
+  {
+    id: 22,
+    image: require('../assets/images/produtos/enlatados22.jpeg')
+  },
+  {
+    id: 23,
+    image: require('../assets/images/produtos/massas23.jpeg')
+  },
+  {
+    id: 24,
+    image: require('../assets/images/produtos/condimentos24.jpeg')
+  },
+  {
+    id: 26,
+    image: require('../assets/images/produtos/fraldas26.jpg')
+  },
+  {
+    id: 26,
+    image: require('../assets/images/produtos/fraldas26.jpg')
+  },
+  {
+    id: 26,
+    image: require('../assets/images/produtos/fraldas26.jpg')
+  },
+  {
+    id: 27,
+    image: require('../assets/images/produtos/perfume27.jpeg')
+  },
+  {
+    id: 28,
+    image: require('../assets/images/produtos/verduras28.jpg')
+  },
+  {
+    id: 29,
+    image: require('../assets/images/produtos/limpeza29.jpeg')
+  },
+  {
+    id: 31,
+    image: require('../assets/images/produtos/pet31.jpg')
+  },
+  {
+    id: 32,
+    image: require('../assets/images/produtos/graos32.jpg')
+  },
+  {
+    id: 33,
+    image: require('../assets/images/produtos/frango33.jpg')
+  },
+  {
+    id: 34,
+    image: require('../assets/images/produtos/carnes34.jpg')
+  },
+  {
+    id: 35,
+    image: require('../assets/images/produtos/peixe35.jpg')
+  },
+  {
+    id: 36,
+    image: require('../assets/images/produtos/congelados36.jpg')
+  },
+  {
+    id: 37,
+    image: require('../assets/images/produtos/sorvete37.jpg')
+  },
+  {
+    id: 38,
+    image: require('../assets/images/produtos/leites38.jpeg')
+  },
+  {
+    id: 39,
+    image: require('../assets/images/produtos/frios39.jpg')
+  },
+  {
+    id: 40,
+    image: require('../assets/images/produtos/frigorifico40.jpeg')
+  },
+  {
+    id: 43,
+    image: require('../assets/images/produtos/sabonete43.jpg')
+  },
+  {
+    id: 44,
+    image: require('../assets/images/produtos/ofertas44.jpg')
+  },
+  {
+    id: 45,
+    image: require('../assets/images/produtos/aveia45.jpg')
+  },
+  {
+    id: 46,
+    image: require('../assets/images/produtos/bolacha46.jpg')
+  },
+  {
+    id: 47,
+    image: require('../assets/images/produtos/cozinha47.jpg')
+  },
+  {
+    id: 48,
+    image: require('../assets/images/produtos/sandalia48.png')
+  },
+  {
+    id: 49,
+    image: require('../assets/images/produtos/milho49.jpeg')
+  },
+  {
+    id: 50,
+    image: require('../assets/images/produtos/beleza50.jpg')
+  },
+  {
+    id: 51,
+    image: require('../assets/images/produtos/margarina51.jpeg')
+  },
+  {
+    id: 52,
+    image: require('../assets/images/produtos/prontos52.jpeg')
+  },
+  {
+    id: 53,
+    image: require('../assets/images/produtos/arroz53.jpeg')
+  },
+  {
+    id: 54,
+    image: require('../assets/images/produtos/cafe54.png')
+  },
+  {
+    id: 55,
+    image: require('../assets/images/produtos/sucos55.jpg')
+  },
+  {
+    id: 56,
+    image: require('../assets/images/produtos/salgadinhos56.jpeg')
+  },
+  {
+    id: 57,
+    image: require('../assets/images/produtos/vinagre57.jpg')
+  },
+  {
+    id: 58,
+    image: require('../assets/images/produtos/dieta58.jpg')
+  },
+  {
+    id: 59,
+    image: require('../assets/images/produtos/cha59.jpeg')
+  },
+  {
+    id: 60,
+    image: require('../assets/images/produtos/veneno60.jpg')
+  },
+  {
+    id: 61,
+    image: require('../assets/images/produtos/talheres61.jpg')
+  },
+  {
+    id: 62,
+    image: require('../assets/images/produtos/absorventes62.jpeg')
+  },
+  {
+    id: 63,
+    image: require('../assets/images/produtos/papelHigienico63.jpeg')
+  },
+  {
+    id: 64,
+    image: require('../assets/images/produtos/higieneBucal64.jpg')
+  },
 ]
